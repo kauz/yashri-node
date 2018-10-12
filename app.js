ca5f11e439ee6e6f3341951300375bcd2d8f84d0
kauz.th <kauzlein@gmail.com>
2018-10-12 18:31:54 +0300

    added vendor prefixes to player-thumbs & compressed css

--- app.js
+++ app.js
@@ -25,7 +25,8 @@ app.use(sassMiddleware({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   indentedSyntax: false, // true = .sass and false = .scss
-  sourceMap: true
+  sourceMap: true,
+  outputStyle: 'compressed'
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
