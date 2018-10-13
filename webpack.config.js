5bc45b6e4333dc56c807a39e6e39e8b288d86cb5
kauzlein <kauzlein@gmail.com>
2018-10-13 18:02:02 +0300

    Changed bundle directory

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     },
 
     output: {
-        path: path.resolve(__dirname, "./public/javascripts"),
+        path: path.resolve(__dirname, "./public/javascripts/bundle"),
         filename: 'main.js'
     },
 
