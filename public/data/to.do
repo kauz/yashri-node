babb2d3b590714225095ab48bd47c1e628a36f8b
kauzlein <kauzlein@gmail.com>
2018-10-14 01:37:57 +0300

    Added audio-analyzer

--- public/data/to.do
+++ public/data/to.do
@@ -1,4 +1,14 @@
+CCTV:
+- Change grid to flexbox?
+- Animations
+- Fix "Uncaught DOMException: Failed to execute 'createMediaElementSource' on 'BaseAudioContext': HTMLMediaElement already connected previously to a different MediaElementSourceNode." bug
+- Output data from audio analyzer
+- Create better UI for controls
+
+
+
+
+
 - Fix styles for shadow DOM & data.buttons (Think how to include autoprefixer)
 ✓ Run webpack --watch on npm dev script
 - Fix TouchCtrl selector
-!!! Finish up CCTV page
--- public/javascripts/audio-analyzer.js
+++ public/javascripts/audio-analyzer.js
@@ -1,19 +1,52 @@
 module.exports = (function () {
 
     return {
-        analyze: function () {
-            let AdioContext = w.AudioContext || w.webkitAudioContext;
+        analyze: function (video) {
 
-                this.context = new AudioContext();
-                this.node = this.context.createScriptProcessor(2048, 1, 1);
-                this.anayzer = this.context.createAnalyser();
-                this.anayzer.smoothingTimeConstant = 0.3;
-                this.anayzer.fftSize = 512;
+            if (!window.AudioContext || window.webkitAudioContext) {
+                let AudioContext = window.AudioContext || window.webkitAudioContext;
+            }
+            let self = this;
 
-                this.bands = new Uint8Array(this.anayzer.frequencyBinCount);
+            this.video = video;
+            // Create audio context
+            this.context = new AudioContext();
+            this.node = this.context.createScriptProcessor(2048, 1, 1);
+
+            // Create audio analyzer
+            this.anayzer = this.context.createAnalyser();
+            // Frequency of analyzer's requiring data
+            this.anayzer.smoothingTimeConstant = 0.3;
+            // Amount of output data
+            this.anayzer.fftSize = 512;
+
+            this.bands = new Uint8Array(this.anayzer.frequencyBinCount);
+
+            // Connect video with audioContext
+            self.source = self.context.createMediaElementSource(self.video);
+            // Connect source with analyzer
+            self.source.connect(self.anayzer);
+            // Connect analyzer with data interface
+            self.anayzer.connect(self.node);
+            // Connect everything with output
+            self.node.connect(self.context.destination);
+            self.source.connect(self.context.destination);
+            // Listen to input data change
+            self.node.onaudioprocess = function () {
+                self.anayzer.getByteFrequencyData(self.bands);
+                if (!self.video.paused) {
+                    if (typeof self.update === 'function') {
+                        return self.update(self.bands);
+                    } else {
+                        return 0;
+                    }
+                }
+            };
+
+            console.log(this);
+            return this;
         }
     }
-
 
 
 })();
